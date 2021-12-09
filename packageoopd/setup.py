fcc6f9b10d775faa7bd5e431c3c1f5cce79b9752
Gagandeep20320 <gagandeep20320@iiitd.ac.in>
2021-12-09 19:07:43 +0530

    Package files added

---
+++ packageoopd/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+setup(name="packageoopd",
+version = "1.3",
+description="This is our project of endsem",
+author="Group7", 
+packages = ['packageoopd'],
+install_requires = [])
