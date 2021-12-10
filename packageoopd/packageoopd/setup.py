08118c0bdf06da33cb0102d6cffc0c1786a9429e
VSriSindhu <sri20326@iiitd.ac.in>
2021-12-10 20:27:09 +0530

    package added

---
+++ packageoopd/packageoopd/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+setup(name="packageoopd",
+version = "2",
+description="This is an assembler",
+author="Group7", 
+packages = ['packageoopd'],
+install_requires = ["multipledispatch", "matplotlib", "abcplus"])
