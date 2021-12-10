56fd395b43d34ed45589ac4ab8b5ee629c3858ca
VSriSindhu <sri20326@iiitd.ac.in>
2021-12-10 20:40:20 +0530

    added whl file

--- packageoopd/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-setup(name="packageoopd",
-version = "1.8",
-description="This is an assembler",
-author="Group7", 
-packages = ['packageoopd'],
-install_requires = ["multipledispatch",  "matplotlib"])
